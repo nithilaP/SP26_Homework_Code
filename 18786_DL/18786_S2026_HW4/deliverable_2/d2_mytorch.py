f115dbeef323e4567e906f90013086590461b6ae
Nithila Poongovan <nithila@poongo.net>
2026-03-21 01:47:16 -0400

    real alexnet implemetnation

--- 18786_DL/18786_S2026_HW4/deliverable_2/d2_mytorch.py
+++ 18786_DL/18786_S2026_HW4/deliverable_2/d2_mytorch.py
@@ -16,7 +16,7 @@ from torch.utils.data import DataLoader
 from torchvision.transforms.functional import to_pil_image
 
 # DELIVERABLE 2: OPTIMIZED CNN CLASS IMPLEMENTATION
-class MyCNN(nn.Module):
+class Baseline(nn.Module):
     def __init__(self, num_output_classes=None):
 
         """
@@ -67,6 +67,104 @@ class MyCNN(nn.Module):
             nn.Dropout(p=0.25),
             nn.Linear(256, self.num_output_classes),
         )
+        
+    def __call__(self, x):
+    
+        return self.forward(x)
+    
+    def forward(self, x):
+
+        """
+        [input]
+        x (torch.tensor)      : (batch_size, in_channels, input_height, input_width)
+
+        [output]
+        output (torch.tensor) : (batch_size, out_channels, output_height, output_width)
+
+        """
+        # print("TEST: forward pass")
+
+        x = self.conv_layers(x)
+        x = self.fc_layers(x)
+
+        return x
+
+# DELIVERABLE 2: OPTIMIZED CNN CLASS IMPLEMENTATION
+class Basic_AlexNET(nn.Module):
+    def __init__(self, num_output_classes=None):
+
+        """
+        My custom CNN, designed for CIFAR-100. 
+
+        Pattern: 
+        - Conv
+        - Activation (ReLU)
+        - Max Pooling
+
+        [input]
+        * num_output_classes   : number of classes for output
+        """
+        super().__init__()
+
+        self.num_output_classes = num_output_classes
+        if (num_output_classes == None):
+            self.num_output_classes = 100        
+
+        # d1 baseline: 2 (conv + batch_norm + relu + pool) + flatten + fc + relu + dropout + fc
+        # alexnet : 5 Conv w batch_norm, 3 FC 
+        # nn.Conv2d: https://docs.pytorch.org/docs/stable/generated/torch.nn.Conv2d.html
+        # nn.BatchNorm2d: https://docs.pytorch.org/docs/stable/generated/torch.nn.BatchNorm2d.html
+        # nn.ReLU: https://docs.pytorch.org/docs/stable/generated/torch.nn.ReLU.html
+        # nn.Linear: https://docs.pytorch.org/docs/stable/generated/torch.nn.Linear.html
+        # nn.Dropout: https://docs.pytorch.org/docs/stable/generated/torch.nn.Dropout.html
+        self.conv_layers = nn.Sequential(
+            
+            # LAYER 1 
+            # Conv(3→96) → BN(96) → ReLU → MaxPool
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(num_features=96),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # LAYER 2 
+            # Conv(96→256) → BN(256) → ReLU → MaxPool
+            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # Conv(256→384) → BN(384) → ReLU
+            nn.Conv2d(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(num_features=384),
+            nn.ReLU(),
+
+            # Conv(384→384) → BN(384) → ReLU
+            nn.Conv2d(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(num_features=384),
+            nn.ReLU(),
+
+            # Conv(384→256) → BN(256) → ReLU → MaxPool
+            nn.Conv2d(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(num_features=256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        self.fc_layers = nn.Sequential(
+            # for flatten before fc layer
+            nn.Flatten(),
+
+            # FC HIDDEN LAYER 1 
+            nn.Linear(256 * 4 * 4, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+
+            # FC OUTPUT LAYER 
+            nn.Linear(4096, 4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.25),
+            nn.Linear(4096, self.num_output_classes),
+        )
     
     def __call__(self, x):
         
@@ -88,6 +186,7 @@ class MyCNN(nn.Module):
         x = self.fc_layers(x)
 
         return x
+
 
 # Train the Model: 
 # -> https://docs.pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
@@ -275,17 +374,26 @@ if __name__ == "__main__":
     # MODEL TRAINING FOR DELIVERABLE 2
 
     # TUNABLE PARAM FOR ABLATION STUDY 
-    batch_size = 64
-    epochs = 10
+    batch_size = 128
+    epochs = 50
     learning_rate = 0.01
     momentum = 0.9
-    weight_decay = 0
+    weight_decay = 0.0005
     scheduler_step_size = 10
     scheduler_gamma = 0.1
 
     # data transformations: experiment with augmentations here (random crop, etc)
-    train_transformation = transforms.Compose([transforms.ToTensor()])
-    test_transformation = transforms.Compose([transforms.ToTensor()])
+    # normalization values used for CIFAR-100: 
+    #   Normalize accorindg to ImageNet values:  mean = [0.485, 0.456, 0.406] and std = [0.229, 0.224, 0.225]
+    # -> https://stackoverflow.com/questions/58151507/why-pytorch-officially-use-mean-0-485-0-456-0-406-and-std-0-229-0-224-0-2
+    # -> https://pytorch.org/vision/stable/models.html
+    train_transformation = transforms.Compose([transforms.ToTensor(), 
+                                               transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+    test_transformation = transforms.Compose([transforms.ToTensor(), 
+                                               transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))])
+
+    # train_transformation_normalized = transforms.Compose([transforms.ToTensor()])
+    # test_transformation = transforms.Compose([transforms.ToTensor()])
 
     # init data loaders & data: https://docs.pytorch.org/vision/0.9/datasets.html#cifar
     train_data = datasets.CIFAR100(root=data_root, train=True, download=True, transform=train_transformation)
@@ -300,15 +408,26 @@ if __name__ == "__main__":
         # device = torch.cuda.current_device()
         device = 'cuda'
     
-    # CNN 
-    my_cnn = MyCNN(num_output_classes=100)
-    cnn, train_loss, train_accuracy, test_loss, test_accuracy = train(net=my_cnn, num_epoch=epochs, learning_rate=learning_rate, momentum=momentum, weight_decay=weight_decay, 
+    # baseline CNN 
+    # my_baseline_cnn = Baseline(num_output_classes=100)
+    # my_baseline_cnn, train_loss, train_accuracy, test_loss, test_accuracy = train(net=my_baseline_cnn, num_epoch=epochs, learning_rate=learning_rate, momentum=momentum, weight_decay=weight_decay, 
+    #                                                                   scheduler_step_size=scheduler_step_size, scheduler_gamma=scheduler_gamma,train_dataloader=train_dataloader, 
+    #                                                                   test_dataloader=test_dataloader, device=device)
+
+    # PLOTTING
+    # num_epochs = len(train_loss)
+    # epochs_axis = [i for i in range(1, num_epochs + 1)]
+    # generate_plots("CNN", epochs_axis, train_loss=train_loss, train_accuracy=train_accuracy, test_loss=test_loss, test_accuracy=test_accuracy)
+    # visualize_preds(my_cnn, "CNN", test_data, train_data.classes, device)
+
+    # baseline CNN 
+    my_basic_alexnet_cnn = Basic_AlexNET(num_output_classes=100)
+    my_basic_alexnet_cnn, train_loss, train_accuracy, test_loss, test_accuracy = train(net=my_basic_alexnet_cnn, num_epoch=epochs, learning_rate=learning_rate, momentum=momentum, weight_decay=weight_decay, 
                                                                       scheduler_step_size=scheduler_step_size, scheduler_gamma=scheduler_gamma,train_dataloader=train_dataloader, 
                                                                       test_dataloader=test_dataloader, device=device)
 
     # PLOTTING
     num_epochs = len(train_loss)
     epochs_axis = [i for i in range(1, num_epochs + 1)]
-    generate_plots("CNN", epochs_axis, train_loss=train_loss, train_accuracy=train_accuracy, test_loss=test_loss, test_accuracy=test_accuracy)
-    visualize_preds(my_cnn, "CNN", test_data, train_data.classes, device)
-
+    generate_plots("AlexNet_CNN", epochs_axis, train_loss=train_loss, train_accuracy=train_accuracy, test_loss=test_loss, test_accuracy=test_accuracy)
+    visualize_preds(my_basic_alexnet_cnn, "AlexNet_CNN", test_data, train_data.classes, device)
