2052d3b1122425bcb4c2e03797fd8a6b3ef44afa
Nithila Poongovan <nithila@poongo.net>
2026-03-22 02:56:09 -0400

    done

---
+++ 18786_DL/18786_S2026_HW4/deliverable_4/deliverable4.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np
+import time
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision import datasets, transforms
+
+def performance_polling(): 
+
+
+if __name__ == "__main__":
