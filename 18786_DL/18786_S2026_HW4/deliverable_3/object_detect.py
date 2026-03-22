3d3a4ea1d2cca3d6ed2e71dbabfd4f29c6e4b1c5
Nithila Poongovan <nithila@poongo.net>
2026-03-22 01:04:34 -0400

    overlap and merge

--- 18786_DL/18786_S2026_HW4/deliverable_3/object_detect.py
+++ 18786_DL/18786_S2026_HW4/deliverable_3/object_detect.py
@@ -297,19 +297,120 @@ def overlap_and_merge_implementation(input_image, image_name, device):
         # Dog: 152 (Chihuahua) to 269 (Mexican hairless)
         # Cat: 281 (tabby) to 293 (Cheetah)
         if (score > 0.5 and ((151 <= prediction <= 268) or (281 <= prediction <= 293))): # hardcoded confidence threshold to 0.3
-            dog_cat_found.append({"image": image_i, "score": score, "prediction": prediction, "subimage_coord": patch_coord[curr_index], "image_label": ResNet18_Weights.DEFAULT.meta["categories"][prediction]})
+
+            pred_label = "dog"
+            if (281 <= prediction<= 293):
+                pred_label = "cat"
+
+            dog_cat_found.append({"image": image_i, "score": score, "prediction": prediction, "subimage_coord": patch_coord[curr_index], "image_label": ResNet18_Weights.DEFAULT.meta["categories"][prediction], "prediction_label": pred_label})
         
         curr_index += 1
 
     # merge boxes that are around each other
     merged_found=[]
+    merged_found_subimages=[]
+    already_merged = [False for i in dog_cat_found]
+    for i, found_object in enumerate(dog_cat_found):
+
+        if (already_merged[i] == True):
+            continue;
+        
+        # set the merged coordinates as the current start box's
+        merge_x_min, merge_y_min, merge_x_max, merge_y_max = found_object["subimage_coord"]
+        
+        # find center for current box for distance calc
+        center_x = 0.5 * (merge_x_min + merge_x_max) 
+        center_y = 0.5 * (merge_y_min + merge_y_max)
+
+        # tracking var for loop
+        x_min, y_min, x_max, y_max = found_object["subimage_coord"]
+        for j in range(i+1, len(dog_cat_found)):
+
+            # if already merged move on. 
+            if (already_merged[j] == True):
+                continue;
     
+            if (found_object["prediction_label"] != dog_cat_found[j]["prediction_label"]):
+                continue;
+    
+            # find center of j box for distance eq
+            merge_x_j_min, merge_y_j_min, merge_x_j_max, merge_y_j_max = dog_cat_found[j]["subimage_coord"]
+            center_j_x = 0.5 * (merge_x_j_min + merge_x_j_max) 
+            center_j_y = 0.5 * (merge_y_j_min + merge_y_j_max)
+
+            # distance eq 
+            distance = ((center_x - center_j_x) ** 2 + (center_y - center_j_y) ** 2) ** 0.5
+
+            if (distance < patch_width):
+
+                # do the merge
+                # Update merge_x_min etc and center_x and center_y
+                merge_x_min = min(merge_x_min, merge_x_j_min)
+                merge_y_min = min(merge_y_min, merge_y_j_min)
+
+                merge_x_max = max(merge_x_max, merge_x_j_max)
+                merge_y_max = max(merge_y_max, merge_y_j_max)
+
+                center_x = 0.5 * (merge_x_min + merge_x_max) 
+                center_y = 0.5 * (merge_y_min + merge_y_max)
+
+                # update merge tracker 
+                already_merged[j] = True
+            
+        merged_vals = (merge_x_min, merge_y_min, merge_x_max, merge_y_max)
+
+        merged_found.append({"image": found_object["image"], "score": found_object["score"], "prediction": found_object["prediction"],
+                             "subimage_coord":merged_vals, "image_label": found_object["image_label"], 
+                             "prediction_label": found_object["prediction_label"]})
+        
+        merged_found_subimages.append(input_image.crop(merged_vals))
+
+        already_merged[i] = True # update for next few iterations
 
     # redo the detection on this
-    
+    dog_cat_found_w_merged_box = []
+    curr_index = 0
+    for image_i in merged_found_subimages:
+        # classify with the model
+        with torch.no_grad(): 
+            
+            # preprocess the image
+            input_tensor = preprocess_transform(image_i)
+            input_batch = input_tensor.unsqueeze(0)
+            input_batch = input_batch.to(device)
+
+            # put image through model -> outputs 1000 scores
+            model_output = baseline_model(input_batch)
+
+            # model_output = unnormalized scores -> run softmas to get probabilities
+            probabilities = F.softmax(model_output[0], dim=0)
+
+            # get index with highest score / energy -> choose as prediction
+            score, prediction = torch.max(probabilities, dim=0)
+            score = score.item() # 
+            prediction = prediction.item()
+
+            print(f"{image_name}:: subimage_prediction: {prediction}| prediction_name: {ResNet18_Weights.DEFAULT.meta['categories'][prediction]}| score: {score}")
+
+
+        # validate detected cat or dog
+        # ImageNet Classes: https://github.com/pytorch/hub/blob/master/imagenet_classes.txt (for torch pretrained models -> ResNet18)
+        # Dog: 152 (Chihuahua) to 269 (Mexican hairless)
+        # Cat: 281 (tabby) to 293 (Cheetah)
+        if (score > 0.5 and ((151 <= prediction <= 268) or (281 <= prediction <= 293))): # hardcoded confidence threshold to 0.3
+
+            pred_label = "dog"
+            if (281 <= prediction <= 293):
+                pred_label = "cat"
+
+            dog_cat_found_w_merged_box.append({"image": image_i, "score": score, "prediction": prediction, "subimage_coord": merged_found[curr_index]["subimage_coord"],
+                                               "image_label": ResNet18_Weights.DEFAULT.meta['categories'][prediction], "prediction_label": pred_label})
+        
+        curr_index += 1
+
     # create bounding box around identified cat and dog
     bbox_creation = ImageDraw.Draw(input_image) 
-    for found_object in dog_cat_found: 
+    for found_object in dog_cat_found_w_merged_box: 
 
         x_min, y_min, x_max, y_max = found_object["subimage_coord"]
         box_xyxy = [x_min, y_min, x_max, y_max] 
@@ -342,5 +443,8 @@ if __name__ == "__main__":
     # run_baseline("../2007_001239.jpg", "dog_image", device)
     # run_baseline("../2008_002152.jpg", "cat_image", device)
 
-    overlapping_patches_implementation("../2007_001239.jpg", "dog_image", device)
-    overlapping_patches_implementation("../2008_002152.jpg", "cat_image", device)
+    # overlapping_patches_implementation("../2007_001239.jpg", "dog_image_overlap", device)
+    # overlapping_patches_implementation("../2008_002152.jpg", "cat_image_overlap", device)
+
+    overlap_and_merge_implementation("../2007_001239.jpg", "dog_image_overlap_and_merge", device)
+    overlap_and_merge_implementation("../2008_002152.jpg", "cat_image_overlap_and_merge", device)
